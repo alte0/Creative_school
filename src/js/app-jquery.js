ab791a41ad90a70bee3c659cd79dc3c51e8ec9c3
Максим <ctpagateji@yandex.ru>
2016-11-16 15:28:58 +0500

    домашка  на 17 ноября

---
+++ src/js/app-jquery.js
@@ -0,0 +1,10 @@
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+$(function(){ 
+	var img = $('.img');
+	if ( img.length ) {
+		console.log('класс .img - Да есть!!!!!!!!!!!!!!!')
+	} else {
+		console.log('Печаль.... класса .img и в помине нету!!!!!!')
+	}
+});
